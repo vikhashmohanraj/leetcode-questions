0db494381404a4ec8b48bc65373f4dc8fc26632c
vikhashmohanraj <vikhashmohanraj2002@gmail.com>
2026-07-25 13:27:56 +0530

    Create DAY8-771-jewels-stone.java

---
+++ DAY8-771-jewels-stone.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char j : jewels.toCharArray()) {
+            for (char s : stones.toCharArray()) {
+                if (j == s) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
