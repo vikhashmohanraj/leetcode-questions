47ad11bf6a5ab8f75324891ec46ca3552625c258
vikhashmohanraj <vikhashmohanraj2002@gmail.com>
2026-07-25 14:03:28 +0530

    Create Lab-6 Event handling.java

---
+++ Lab-6 Event handling.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-----------------------------------");
+        System.out.println("            Simple Calculator");
+        System.out.println("-----------------------------------");
+
+        System.out.print("First Number : ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Second Number : ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Click : ");
+        String operation = sc.next();
+
+        double result = 0;
+        boolean valid = true;
+
+        try {
+            switch (operation) {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Sub":
+                    result = num1 - num2;
+                    break;
+                case "Mul":
+                    result = num1 * num2;
+                    break;
+                case "Divide":
+                    if (num2 == 0) {
+                        System.out.println("Result : Cannot divide by zero");
+                        valid = false;
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
