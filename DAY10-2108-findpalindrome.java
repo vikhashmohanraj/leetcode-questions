4a3aeaf81cf471da9bb972122bfef32712aff47f
vikhashmohanraj <vikhashmohanraj2002@gmail.com>
2026-07-25 11:28:59 +0530

    Create DAY10-2108-findpalindrome.java

---
+++ DAY10-2108-findpalindrome.java
@@ -0,0 +1,11 @@
+ublic class Solution {
+    public static String firstPalindrome(String[] words) {
+        for (String word : words) {
+            StringBuilder reversed = new StringBuilder(word).reverse();
+            if (word.equals(reversed.toString())) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
